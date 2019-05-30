bdd0df6e702923f7c0b4512106bbe5d9c00bde2d
M Banks <montensia1@outlook.com>
2019-05-30 14:04:13 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,40 @@
+require 'pry'
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def self.find_or_create_by_name(name)
+    found_arist = @@all.find{ |artist| artist.name == name}
+    if found_arist == nil
+      self.new(name)
+    else
+      found_arist
+    end
+  end
+  
+  def print_songs
+    binding.pry
+    puts @songs
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(filename)
+    file_array = filename.split(" - ")
+    new_song = Song.new(file_array[1])
+
+    artist = Artist.find_or_create_by_name(file_array[0])
+
+    new_song.artist = artist
+    new_song
+  end
+  
+
+end
+
