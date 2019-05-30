1b12b489495b631ce68378fd46bbf146e7f473d9
Angela Mumba <malaika.mumba@gmail.com>
2019-05-30 19:22:34 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,43 +1,81 @@
-class Artist 
-  
-  attr_accessor :name,:songs 
-  
+class Artist
   @@all = []
-  
-  def initialize(name)
-    @name = name 
-    @songs = []
-  end 
-  
-  def self.all
-    @@all
-  end 
-  
-  def add_song(song)
-    song.artist = self
-    @songs << song 
-  end
-  
-  def save 
-    @@all << self
-  end 
+  attr_accessor :name, :songs
 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all    
+  end
+
+  def add_song(song)
+    @songs << song
+  end
 
   def self.find_or_create_by_name(name)
-    found_artist = @@all.find{ |artist| artist.name == name}
-    if found_artist == nil
-      self.new(name)
-    else
-      found_artist
-    end
+    self.find(name) ? self.find(name) : self.create(name)
   end
-  
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
+  end
+
+  def save
+    @@all << self
+  end
+
   def print_songs
-    songs.each do |song| 
-  puts song.name
-    
-    end 
+    songs.each {|song| puts song.name}
   end
-end 
+end
+# class Artist 
+  
+#   attr_accessor :name,:songs 
+  
+#   @@all = []
+  
+#   def initialize(name)
+#     @name = name 
+#     @songs = []
+#   end 
+  
+#   def self.all
+#     @@all
+#   end 
+  
+#   def add_song(song)
+#     song.artist = self
+#     @songs << song 
+#   end
+  
+#   def save 
+#     @@all << self
+#   end 
+
+
+#   def self.find_or_create_by_name(name)
+#     found_artist = @@all.find{ |artist| artist.name == name}
+#     if found_artist == nil
+#       self.new(name)
+#     else
+#       found_artist
+#     end
+#   end
+  
+#   def print_songs
+#     songs.each do |song| 
+#   puts song.name
+    
+#     end 
+#   end
+# end 
 
 # I am working on the Collaborating lab and the method that associates the new instance to song to with a found artist keeps failing. I even tried the formular from the  learn lab read me. Here is my code and the error. Can someone get on a call to help me?
--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,68 @@
-require 'pry'
+
 class Song
   attr_accessor :name, :artist
 
-  @@all = []
-
   def initialize(name)
     @name = name
-   
   end
-  
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    # self.artist.add_song(self)
-    #name
-  end
-  
-  
+
+  #   def self.new_by_filename(filename)
+  #   parts = filename.split(" - ")
+  #   artist_name = parts[0]
+  #   song_name = parts[1].gsub(".mp3", "")
+
+  #   song = Song.new(song)
+  #   song.name = song_name
+  #   song.artist_name = artist
+  #   song
+    
+  # end
   
   def self.new_by_filename(filename)
-    file_array = filename.split(" - ")
-    binding.pry
-  #make a new song from the parsed filename
-    new_song = Song.new(file_array[1])
-     
-    #use info from parsed filename to find artist
-    artist = Artist.find_or_create_by_name(file_array[0])
-    #binding.pry
-    #associate the newly created song with the found or created artist
-    # new_song.artist = artist
-    artist.add_song(new_song)
-    
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+end
+# require 'pry'
+# class Song
+#   attr_accessor :name, :artist
+
+#   @@all = []
+
+#   def initialize(name)
+#     @name = name
+#     @@all << self 
+#   end
+  
+#   def artist_name=(name)
+#     self.artist = Artist.find_or_create_by_name(name)
+#     # self.artist.add_song(self)
+#     #name
+#   end
+  
+  
+  
+#   def self.new_by_filename(filename)
+#     file_array = filename.split(" - ")
+#     # binding.pry
+#   #make a new song from the parsed filename
+#     new_song = Song.new(file_array[1])
+     
+#     #use info from parsed filename to find artist
+#     artist = Artist.find_or_create_by_name(file_array[0])
+#     #binding.pry
+#     #associate the newly created song with the found or created artist
+#     new_song.artist = artist
+#     artist.add_song(new_song)
+    
+#   end
 
   # def self.new_by_filename(filename)
   #   parts = filename.split(" - ")
@@ -45,79 +77,79 @@ class Song
 
   #   song = Song.new(song)
   #   song.name = song_name
-  #   song.artist_name = artist_name
+  #   song.artist_name = artist
   #   song
     
   # end
   
 
-  def self.find_by_artist(artist)
-    Song.all.select do | song |
-      song.artist == artist
-    end
-  end
-  
-  def self.all
-    @@all
-  end
-
-  
-  def save
-    @@all << self
-    self
-  end
-end
-
-
-# class Song
-
-#   attr_accessor :name, :artist
-
-#   def initialize(name)
-#     @name = name
+#   def self.find_by_artist(artist)
+#     Song.all.select do | song |
+#       song.artist == artist
+#     end
 #   end
-
-#   def self.new_by_filename(file_name)
-#     parts = file_name.split(" - ")
-#     artist_name = parts[0]
-#     song_name = parts[1].gsub(".mp3", "")
-#     new_song = Song.new(parts[1])
-    
-   
-#     # self.new(name)
-#     # song = self.create
-#     # song.name = song_name
-#     # song.artist_name = artist_name
-#     # song
-#   end
-
-# end
-
-# class Song
-#   attr_accessor :name, :artist
-#   @@all = []
-#   def initialize(name)
-#     @name = name
-#     @@all << self 
-#   end
-  
   
 #   def self.all
 #     @@all
-#   end 
-  
-#   def self.new_by_filename(filename)
-#     file_array = filename.split(" - ")
-    
-#   #make a new song from the parsed filename
-#     new_song = Song.new(file_array[1])
-     
-#     #use info from parsed filename to find artist
-#     artist = Artist.find_or_create_by_name(file_array[0])
-    
-#     #associate the newly created song with the found or created artist
-#     new_song.artist = artist
-#     new_song 
-   
 #   end
-# end 
+
+  
+#   def save
+#     @@all << self
+#     self
+#   end
+# end
+
+
+# # class Song
+
+# #   attr_accessor :name, :artist
+
+# #   def initialize(name)
+# #     @name = name
+# #   end
+
+# #   def self.new_by_filename(file_name)
+# #     parts = file_name.split(" - ")
+# #     artist_name = parts[0]
+# #     song_name = parts[1].gsub(".mp3", "")
+# #     new_song = Song.new(parts[1])
+    
+   
+# #     # self.new(name)
+# #     # song = self.create
+# #     # song.name = song_name
+# #     # song.artist_name = artist_name
+# #     # song
+# #   end
+
+# # end
+
+# # class Song
+# #   attr_accessor :name, :artist
+# #   @@all = []
+# #   def initialize(name)
+# #     @name = name
+# #     @@all << self 
+# #   end
+  
+  
+# #   def self.all
+# #     @@all
+# #   end 
+  
+# #   def self.new_by_filename(filename)
+# #     file_array = filename.split(" - ")
+    
+# #   #make a new song from the parsed filename
+# #     new_song = Song.new(file_array[1])
+     
+# #     #use info from parsed filename to find artist
+# #     artist = Artist.find_or_create_by_name(file_array[0])
+    
+# #     #associate the newly created song with the found or created artist
+# #     new_song.artist = artist
+# #     new_song 
+   
+# #   end
+# # end 
